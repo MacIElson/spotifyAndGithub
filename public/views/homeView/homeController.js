8d090483b8f5d1736f12b53509ad6abe053c8890
Samantha Kumarasena <samantha.kumarasena@students.olin.edu>
2016-03-06 13:37:08 -0500

    List user playlists, backup buttons (not functional yet)

--- public/views/homeView/homeController.js
+++ public/views/homeView/homeController.js
@@ -7,11 +7,39 @@ angular.module('myApp.homeView', ['ngRoute'])
   });
 }])
 
-.controller('homeController', ['$scope','$window', function($scope,$window) {
+.controller('homeController', ['$scope','$window', '$http', function($scope,$window, $http) {
 	console.log("homeController loaded");
+
+
+    $scope.user = {};
+    $scope.playlists = {};
 
 	$scope.logout = function (){
     	//$location.path( '/auth/spotify' );
     	$window.location.href = '/auth/logout';
 	};
+
+    $http.get('/getCurrentUser')
+        .success(function(data) {
+            $scope.user = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        })
+
+    $http.get('/getCurrentUserPlaylists')
+        .success(function(data){
+            $scope.playlists = data;
+            console.log(data)
+        })
+        .error(function(data) {
+            console.log('Error: ' + data)
+        })
+
+    $scope.backupPlaylist = function(playlist) {
+        console.log('Backing up playlist ' + playlist.name + '!')
+    }
+
+    console.log($scope);
 }]);
